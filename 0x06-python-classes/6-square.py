8869616f909a35058aa95353ffa6817e20c9ff7b
JayJay707 <juliette.mesnil35@gmail.com>
2022-05-17 16:30:29 +0200

    Update task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,7 +6,13 @@ class Square:
     """class Square"""
     def __init__(self, size=0, position=(0, 0)):
         """initialize method"""
-        self.__size = size
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
         self.__position = position
 
     def area(self):
