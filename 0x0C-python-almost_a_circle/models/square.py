1b6567ca3bb3b07e9e9da3ac8cc7b8f83fc7faf7
JayJay707 <juliette.mesnil35@gmail.com>
2022-06-07 11:13:19 +0200

    Added 0x0C-python-almost_a_circle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Square Module"""
+from .rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class that inherits from Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Init method"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    @property
+    def size(self):
+        """Size getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Size setter"""
+        self.height = value
+        self.width = value
+
+    def __str__(self):
+        """Returns a string representation of the square"""
+        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id, self.x,
+                                                         self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        """Updates attributes"""
+        i = 0
+        if args:
+            for arg in args:
+                if i == 0:
+                    self.id = arg
+                if i == 1:
+                    self.size = arg
+                if i == 2:
+                    self.x = arg
+                if i == 3:
+                    self.y = arg
+                i += 1
+        else:
+            for arg in kwargs:
+                setattr(self, arg, kwargs.get(arg))
+
+    def to_dictionary(self):
+        """Returns a dictionary representation of the square"""
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+        
